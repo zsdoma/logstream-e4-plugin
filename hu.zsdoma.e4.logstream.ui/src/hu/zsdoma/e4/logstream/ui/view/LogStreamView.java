b6ca2553b6bc9092d04e1c9b7f6888d67d39fb35
Zsolt Doma <zsolt.doma@gmail.com>
2015-05-25 14:20:47 +0200

    multiple view instance

--- hu.zsdoma.e4.logstream.ui/src/hu/zsdoma/e4/logstream/ui/view/LogStreamView.java
+++ hu.zsdoma.e4.logstream.ui/src/hu/zsdoma/e4/logstream/ui/view/LogStreamView.java
@@ -2,7 +2,9 @@ package hu.zsdoma.e4.logstream.ui.view;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
@@ -22,6 +24,9 @@ import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
@@ -36,6 +41,8 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
 
   private static final int COLUMN_INDEX_TIME = 0;
 
+  private static final String[] tableTitles = new String[] { "Time", "Message" };
+
   private LogStreamService logStreamService;
 
   private Table table;
@@ -46,16 +53,56 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
 
   private IAction closeAction;
 
-  private static final String[] tableTitles = new String[] { "Time", "Message" };
-
   private SimpleDateFormat simpleDateFormat;
 
   private Composite defaultCompositeView;
 
   private Composite parent;
 
+  private int logViewId = 1;
+
+  private Map<Integer, IViewPart> pagesMap = new HashMap<>();
+
+  private IAction newViewAction;
+
   public LogStreamView() {
     simpleDateFormat = new SimpleDateFormat();
+  }
+
+  private void addTableColumn(final String title) {
+    TableColumn column = new TableColumn(table, SWT.NONE);
+    column.setText(title);
+  }
+
+  private void clearDefaultView() {
+    if (hasDefaultCompositeComponent()) {
+      defaultCompositeView.dispose();
+    }
+  }
+
+  private void clearTable() {
+    if (hasTable()) {
+      table.getDisplay().syncExec(() -> {
+        table.removeAll();
+      });
+      table.dispose();
+    }
+  }
+
+  private void createActionBar() {
+    IActionBars bars = getViewSite().getActionBars();
+    IToolBarManager toolBarManager = bars.getToolBarManager();
+    createToolBar(toolBarManager);
+    // IMenuManager menuManager = bars.getMenuManager();
+    // createMenu(menuManager);
+  }
+
+  private void createActions() {
+    createCloseAction();
+    createOpenFileAction();
+    createNewViewAction();
+    createCloseAction();
+    createClearMenu();
   }
 
   private void createClearMenu() {
@@ -72,10 +119,49 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
         .getImageDescriptor(ISharedImages.IMG_ETOOL_CLEAR));
   }
 
+  private void createCloseAction() {
+    closeAction = new Action() {
+      @Override
+      public void run() {
+        fileFollowingStop();
+      }
+    };
+    closeAction.setText("Close");
+    closeAction.setToolTipText("Stop following and close file.");
+    closeAction.setImageDescriptor(
+        PlatformUI.getWorkbench().getSharedImages()
+            .getImageDescriptor(ISharedImages.IMG_ELCL_REMOVE));
+  }
+
+  private void createDefaultView() {
+    if (!parent.isDisposed() && !hasDefaultCompositeComponent()) {
+      defaultCompositeView = new Composite(parent, SWT.NONE);
+      defaultCompositeView.setLayout(new FillLayout());
+      Label titleLabel = new Label(defaultCompositeView, SWT.CENTER);
+      titleLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+      titleLabel.setText("No file opened");
+      titleLabel.setForeground(parent.getDisplay().getSystemColor(SWT.COLOR_GRAY));
+    }
+  }
+
   private void createMenu(final IMenuManager menuManager) {
     menuManager.add(clearAction);
     menuManager.add(new Separator());
     menuManager.add(openFileAction);
+  }
+
+  private void createNewViewAction() {
+    newViewAction = new Action() {
+      @Override
+      public void run() {
+        newLogStreamView();
+      }
+    };
+    newViewAction.setText("New");
+    newViewAction.setToolTipText("Open new View");
+    newViewAction.setImageDescriptor(
+        PlatformUI.getWorkbench().getSharedImages()
+            .getImageDescriptor(ISharedImages.IMG_TOOL_NEW_WIZARD));
   }
 
   private void createOpenFileAction() {
@@ -91,20 +177,6 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
         PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJ_FILE));
   }
 
-  private void createCloseAction() {
-    closeAction = new Action() {
-      @Override
-      public void run() {
-        fileFollowingStop();
-      }
-    };
-    closeAction.setText("Close");
-    closeAction.setToolTipText("Stop following and close file.");
-    closeAction.setImageDescriptor(
-        PlatformUI.getWorkbench().getSharedImages()
-            .getImageDescriptor(ISharedImages.IMG_ELCL_REMOVE));
-  }
-
   @Override
   public void createPartControl(final Composite parent) {
     this.parent = parent;
@@ -113,21 +185,14 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
     createActionBar();
   }
 
-  private void createTableView() {
-    if (!parent.isDisposed()) {
-      createTable();
-    }
-  }
-
-  private void createDefaultView() {
-    if (!parent.isDisposed() && !hasDefaultCompositeComponent()) {
-      defaultCompositeView = new Composite(parent, SWT.NONE);
-      defaultCompositeView.setLayout(new FillLayout());
-      Label titleLabel = new Label(defaultCompositeView, SWT.CENTER);
-      titleLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-      titleLabel.setText("No file opened");
-      titleLabel.setForeground(parent.getDisplay().getSystemColor(SWT.COLOR_GRAY));
-    }
+  private TableItem createRow(final LoggerLineDTO loggerLineDTO) {
+    TableItem tableItem;
+    tableItem = new TableItem(table, SWT.NONE);
+    System.out.println(loggerLineDTO.getMessage());
+    tableItem.setText(COLUMN_INDEX_TIME,
+        simpleDateFormat.format(new Date(loggerLineDTO.getTimestamp())));
+    tableItem.setText(COLUMN_INDEX_MESSAGE, loggerLineDTO.getMessage());
+    return tableItem;
   }
 
   private void createTable() {
@@ -144,28 +209,24 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
     }
   }
 
-  private void createActionBar() {
-    IActionBars bars = getViewSite().getActionBars();
-    IToolBarManager toolBarManager = bars.getToolBarManager();
-    createToolBar(toolBarManager);
-    // IMenuManager menuManager = bars.getMenuManager();
-    // createMenu(menuManager);
+  private void createTableView() {
+    if (!parent.isDisposed()) {
+      createTable();
+    }
   }
 
-  private void createActions() {
-    createCloseAction();
-    createOpenFileAction();
-    createCloseAction();
-    createClearMenu();
-  }
-
-  private void addTableColumn(final String title) {
-    TableColumn column = new TableColumn(table, SWT.NONE);
-    column.setText(title);
+  private String createTitleName(final String filePath) {
+    int length = filePath.length();
+    if (length > Constants.MAX_TITLE_LENGTH) {
+      return filePath.substring(length - Constants.MAX_TITLE_LENGTH);
+    } else {
+      return filePath;
+    }
   }
 
   private void createToolBar(final IToolBarManager toolBarManager) {
     toolBarManager.add(openFileAction);
+    toolBarManager.add(newViewAction);
     toolBarManager.add(clearAction);
     toolBarManager.add(closeAction);
   }
@@ -173,6 +234,44 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
   @Override
   public void dispose() {
     fileFollowingStop();
+  }
+
+  private void fileFollowingStart(final String filePath) {
+    clearDefaultView();
+    createTableView();
+
+    logStreamService = new LogStreamServiceImpl(filePath);
+    logStreamService.registerCallback(this);
+    logStreamService.start();
+    setPartName(createTitleName(filePath));
+  }
+
+  private void fileFollowingStop() {
+    clearTable();
+    createDefaultView();
+    if (logStreamService != null) {
+      logStreamService.stop();
+      logStreamService.unregisterCallback(this);
+    }
+    setPartName(Constants.DEFAULT_TITLE);
+  }
+
+  private boolean hasDefaultCompositeComponent() {
+    return (defaultCompositeView != null) && !defaultCompositeView.isDisposed();
+  }
+
+  private boolean hasTable() {
+    return (table != null) && !table.isDisposed();
+  }
+
+  private void newLogStreamView() {
+    try {
+      IViewPart showView = getViewSite().getPage().showView(
+          "hu.zsdoma.e4.logstream.ui.view.LogStreamView",
+          String.valueOf(logViewId++), IWorkbenchPage.VIEW_VISIBLE);
+    } catch (PartInitException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
@@ -186,23 +285,12 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
         if (tableItem != null) {
           table.showItem(tableItem);
         }
-      } );
+      });
     }
-  }
-
-  private TableItem createRow(LoggerLineDTO loggerLineDTO) {
-    TableItem tableItem;
-    tableItem = new TableItem(table, SWT.NONE);
-    System.out.println(loggerLineDTO.getMessage());
-    tableItem.setText(COLUMN_INDEX_TIME,
-        simpleDateFormat.format(new Date(loggerLineDTO.getTimestamp())));
-    tableItem.setText(COLUMN_INDEX_MESSAGE, loggerLineDTO.getMessage());
-    return tableItem;
   }
 
   @Override
   public void setFocus() {
-    System.out.println("setFocus");
   }
 
   private void showFileChooser() {
@@ -222,58 +310,6 @@ public class LogStreamView extends ViewPart implements LogStreamCallback {
     }
     fileFollowingStop();
     fileFollowingStart(filePath);
-  }
-
-  private void fileFollowingStart(final String filePath) {
-    clearDefaultView();
-    createTableView();
-
-    logStreamService = new LogStreamServiceImpl(filePath);
-    logStreamService.registerCallback(this);
-    logStreamService.start();
-    setPartName(createTitleName(filePath));
-  }
-
-  private void clearDefaultView() {
-    if (hasDefaultCompositeComponent()) {
-      defaultCompositeView.dispose();
-    }
-  }
-
-  private String createTitleName(String filePath) {
-    int length = filePath.length();
-    if (length > Constants.MAX_TITLE_LENGTH) {
-      return filePath.substring(length - Constants.MAX_TITLE_LENGTH);
-    } else {
-      return filePath;
-    }
-  }
-
-  private void fileFollowingStop() {
-    clearTable();
-    createDefaultView();
-    if (logStreamService != null) {
-      logStreamService.stop();
-      logStreamService.unregisterCallback(this);
-    }
-    setPartName(Constants.DEFAULT_TITLE);
-  }
-
-  private void clearTable() {
-    if (hasTable()) {
-      table.getDisplay().syncExec(() -> {
-        table.removeAll();
-      } );
-      table.dispose();
-    }
-  }
-
-  private boolean hasTable() {
-    return table != null && !table.isDisposed();
-  }
-  
-  private boolean hasDefaultCompositeComponent() {
-    return defaultCompositeView != null && !defaultCompositeView.isDisposed();
   }
 
 }
